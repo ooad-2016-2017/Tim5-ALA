d1cc06ab403945219cd5cf751ec1e0c8dbc43023
adnanel <adnanel94@gmail.com>
2017-05-10 23:54:23 +0200

    Kreirao hrpu fajlova

---
+++ Projekat/frms/frms/Models/Korisnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frms.Models
+{
+    class Korisnik
+    {
+        public int ID { get; set; }
+
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Username { get; set; }
+
+
+    }
+}
